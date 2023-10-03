5fd1747f742bf7085f923728746302d83a1ea3e0
Peyman Peirovifar <peirovifar@sw-motion.cn>
2023-10-03 10:56:06 +0330

    [fix] I made mistake in calculating the parameter prb_count for service urllc and used subcariers by mistake

--- energy_model.py
+++ energy_model.py
@@ -18,6 +18,30 @@ power_DU_RU_max = 200
 
 services_count = 3
 
+# def processing_gops_calculator(modulation_idx):
+#
+#     cj2 = const.dff[:, 0]
+#     cj = np.copy(cj2)
+#     actualvalue = np.array([BW, modulation_idx, const.antennas_per_ru, 6, 1])
+#     actToRef = actualvalue / const.refvalue
+#     dff2 = const.dff[:, 1:]
+#     cjj = np.ones(16)
+#     actualvalue_user = np.array([BW_user, modulation_idx, const.antennas_per_ru, 6, 1])
+#     actToRefUser = actualvalue_user / const.refvalue
+#     # print(cjj)
+#
+#     for i in range(16):
+#         for j in range(5):
+#             if i < 11:
+#                 cjj[i] *= pow(actToRef[j], dff2[i, j])
+#             elif i !=15:
+#                 cjj[i] *= pow(actToRefUser[j], dff2[i, j])
+#             else:
+#                 cjj[i] *= pow(actToRef[j], dff2[i, j])
+#     cj *= cjj
+#
+#     return 0
+
 
 # equal allocated number of subcarriers per user
 def service_energy_calculater(service,frame,const):
@@ -52,9 +76,9 @@ const = Constants()
 
 frame = Frame(numerology, True, bandwidth)
 test_duration=100
-subcarrier_count = frame.max_prb_count*8
+prb_count = frame.max_prb_count
 user_count=15
-ser = Service_urllc(user_count,subcarrier_count,test_duration,const)
+ser = Service_urllc(user_count,prb_count,test_duration,const)
 print(ser.target_transfer_interval_value)
 ser.prb_per_user_count
 print(ser.slot_per_user)
